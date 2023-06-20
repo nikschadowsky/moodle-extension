cea5c3d1dc0b71834bf72dfaeda0dc4a975e732a
nikschadowsky <n.s-schadowsky@t-online.de>
2023-06-20 20:44:53 +0200

    initial commit

---
+++ src/extension.js
@@ -0,0 +1,11 @@
+
+links = document.getElementsByClassName("aalink")
+
+for(elem of links){
+    
+}
+
+console.log("heyho");
+
+// replace homepage link
+document.getElementsByClassName("navbar-brand aabtn has-logo")[0].href ="https://moodle2.nordakademie.de/my/"
