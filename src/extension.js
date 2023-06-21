994694325083d8062133d1416ed2547d7c8e1b74
Nik Schadowsky <n.s-schadowsky@t-online.de>
2023-06-21 10:24:58 +0200

    files not opening in new windows anymore

--- src/extension.js
+++ src/extension.js
@@ -1,11 +1,19 @@
 
+console.log("moodle-extension active");
+
 links = document.getElementsByClassName("aalink")
 
-for(elem of links){
+for(let elem of links){
+
+    elem.setAttribute("onclick", "");
     
+    elem.setAttribute("href", elem.getAttribute("href") +"&redirect=1");
+    elem.setAttribute("target", "_blank");  
+    elem.setAttribute("rel", "noreferrer noopener");  
+    elem.replaceWith(elem.cloneNode(true))
 }
 
-console.log("heyho");
 
 // replace homepage link
 document.getElementsByClassName("navbar-brand aabtn has-logo")[0].href ="https://moodle2.nordakademie.de/my/"
+
