22415e04178216b557220c5530b28d384cacd057
nikschadowsky <n.s-schadowsky@t-online.de>
2023-10-25 22:04:37 +0200

    force download now opens newtab

--- src/extension.js
+++ src/extension.js
@@ -3,14 +3,25 @@ console.log("moodle-extension active");
 
 links = document.getElementsByClassName("aalink")
 
-for(let elem of links){
+for (let elem of links) {
 
     elem.setAttribute("onclick", "");
-    
-    elem.setAttribute("href", elem.getAttribute("href") +"&redirect=1");
-    elem.setAttribute("target", "_blank");  
-    elem.setAttribute("rel", "noreferrer noopener");  
+
+    elem.setAttribute("href", elem.getAttribute("href") + "&redirect=1");
+    elem.setAttribute("target", "_blank");
+    elem.setAttribute("rel", "noreferrer noopener");
     elem.replaceWith(elem.cloneNode(true))
+}
+
+
+files = document.getElementsByClassName("fp-filename-icon");
+
+for (let elem of files) {
+    elem.childNodes.forEach(element => {
+        if (element.tagName === 'A') {
+            element.href = element.href.replace('forcedownload=1', 'forcedownload=0');
+            element.target = '_blank';
+    }});
 }
 
 
